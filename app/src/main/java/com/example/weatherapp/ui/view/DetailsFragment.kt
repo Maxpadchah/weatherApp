b1f27cbc584453ff1787e86ce8f00fbe9aa91a95
Максим <padchah-90-00@mail.ru>
2021-06-07 00:41:20 +0300

    Update

--- app/src/main/java/com/example/weatherapp/ui/view/DetailsFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/view/DetailsFragment.kt
@@ -1,40 +1,69 @@
 package com.example.weatherapp.ui.view
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentDetailsBinding
 import com.example.weatherapp.model.Weather
+import com.example.weatherapp.model.WeatherDTO
+
+
 
 class DetailsFragment : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+    private lateinit var weatherBundle: Weather
+
+    private val onLoadListener: WeatherLoader.WeatherLoaderListener =
+        object : WeatherLoader.WeatherLoaderListener {
+
+            override fun onLoaded(weatherDTO: WeatherDTO) {
+                displayWeather(weatherDTO)
+            }
+
+            override fun onFailed(throwable: Throwable) {
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
-        return binding.getRoot()
+        return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let { weather ->
-            weather.city.also { city ->
-                binding.cityField.text = city.city
-                binding.updatedField.text = String.format(
-                    getString(R.string.city_coordinates),
-                    city.lat.toString(),
-                    city.lon.toString()
-                )
-                binding.currentTemperatureField.text = weather.temperature.toString()
-            }
+        weatherBundle = arguments?.getParcelable(BUNDLE_EXTRA) ?: Weather()
+
+        binding.main.visibility = View.GONE
+        binding.loadingLayout.visibility = View.VISIBLE
+        val loader = WeatherLoader(onLoadListener, weatherBundle.city.lat, weatherBundle.city.lon)
+        loader.loadWeather()
+    }
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding) {
+            main.visibility = View.VISIBLE
+            loadingLayout.visibility = View.GONE
+            val city = weatherBundle.city
+            cityField.text = city.city
+            updatedField.text = String.format(
+                getString(R.string.city_coordinates),
+                city.lat.toString(),
+                city.lon.toString()
+            )
+            weatherCondition.text = weatherDTO.fact?.condition
+            currentTemperatureField.text = weatherDTO.fact?.temp.toString()
+            detailsFieldHumidity.text = weatherDTO.fact?.feels_like.toString()
         }
     }
 
@@ -53,4 +82,6 @@ class DetailsFragment : Fragment() {
             return fragment
         }
     }
+
+
 }
